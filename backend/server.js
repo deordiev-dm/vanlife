d7acd4e22a20841ef1613ca80d422ef23fce1efa
deordiev-dm <dimadeordiev2002@gmail.com>
2024-10-10 17:01:45 +0300

    Add MongoDB connection setup in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API is running');

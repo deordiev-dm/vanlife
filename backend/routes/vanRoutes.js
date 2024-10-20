4f42405133066e7eefcfffe6906b76ca5c960b66
deordiev-dm <dimadeordiev2002@gmail.com>
2024-10-20 18:55:06 +0300

    Refactor vanController to add editVanData function

--- backend/routes/vanRoutes.js
+++ backend/routes/vanRoutes.js
@@ -1,11 +1,12 @@
 const experss = require('express');
-const { getAllVans, getVanById, addVan } = require('../controllers/vanController');
+const { getAllVans, getVanById, addVan, editVanData } = require('../controllers/vanController');
 const { getVanReviews } = require('../controllers/reviewController');
 
 const router = experss.Router();
 
 router.get('/', getAllVans);
 router.post('/', addVan);
+router.put('/:vanId', editVanData);
 router.get('/:id', getVanById);
 router.get('/:vanId/reviews', getVanReviews);
 

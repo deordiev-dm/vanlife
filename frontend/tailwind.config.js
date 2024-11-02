9d00e879e862826df1aa9cf020c66da3cd0d75fc
deordiev-dm <dimadeordiev2002@gmail.com>
2024-11-02 11:07:52 +0200

    Extend tailwind config file to include container styles
    - Remove unnecessary comma from main.tsx

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: "1.5rem",
+        screens: {
+          sm: "100%",
+          md: "100%",
+          lg: "100%",
+          xl: "1200px",
+        },
+      },
+    },
   },
   plugins: [],
 };

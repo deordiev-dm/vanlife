eeb76f7de52fc7920f604331f936bc8752044507
deordiev-dm <dimadeordiev2002@gmail.com>
2024-11-02 12:15:17 +0200

    Refactor Homepage.tsx
    - Add container
    - Make text bigger on wider screens
    - Change spacing between elements
    - Change background image
    - Add svh units to improve mobile layout

--- frontend/src/pages/marketing/Homepage.tsx
+++ frontend/src/pages/marketing/Homepage.tsx
@@ -4,20 +4,21 @@ import bg from "@/assets/img/homepage.jpg";
 function Homepage() {
   return (
     <main
-      className="flex flex-col justify-center bg-cover bg-center px-6 py-16 text-white"
+      className="flex flex-col justify-center bg-cover bg-center"
       style={{ backgroundImage: `url(${bg})` }}
     >
-      <h1 className="mb-4 text-balance text-3xl font-extrabold">
-        You got the travel plans, we got the travel vans.
-        
-      </h1>
-      <p className="mb-14 font-medium">
-        Add adventure to your life by joining the #vanlife movement. Rent the
-        perfect van to make your perfect road trip.
-      </p>
-      <Button as="a" to="/vans" colors="orange">
-        Find your van
-      </Button>
+      <div className="container space-y-8 py-16 text-white">
+        <h1 className="mb-12 text-balance text-3xl font-extrabold tracking-tight md:text-4xl 2xl:text-5xl">
+          You got the travel plans, we got the travel vans.
+        </h1>
+        <p className="md:text-xl 2xl:text-2xl">
+          Add adventure to your life by joining the #vanlife movement. Rent the
+          perfect van to make your perfect road trip.
+        </p>
+        <Button as="a" to="/vans" colors="orange">
+          Find your van
+        </Button>
+      </div>
     </main>
   );
 }

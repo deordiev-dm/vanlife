4ff87e854ed11481b116752dc00880e56d6ad885
deordiev-dm <dimadeordiev2002@gmail.com>
2024-11-30 12:14:13 +0200

    Apply stashed changes to main branch

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,29 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: "0.0.0.0", // Allow access from your local network
-  },
-});
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const BACKEND_URL = env.VITE_BACKEND_URL;
+
+  return defineConfig({
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+        "@components": path.resolve(__dirname, "./src/components/"),
+        "@features": path.resolve(__dirname, "./src/features/"),
+        "@pages": path.resolve(__dirname, "./src/pages/"),
+      },
+    },
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target: BACKEND_URL,
+          changeOrigin: true,
+        },
+      },
+    },
+  });
+};
